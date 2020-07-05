40e9fd2d67a6a32ff333c0b163e3514bde10681a
dnailz <dnailzb@outlook.com>
2020-07-05 11:50:21 +0800

    autosave

--- rustlings/exercises/error_handling/errorsn.rs
+++ rustlings/exercises/error_handling/errorsn.rs
@@ -24,8 +24,10 @@ use std::io;
 // PositiveNonzeroInteger is a struct defined below the tests.
 fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, Box<dyn error::Error>> {
     let mut line = String::new();
-    b.read_line(&mut line).map_err(|e| Box::new(e));
-    let num: i64 = line.trim().parse().map_err(|e| Box::new(e));
+    b.read_line(&mut line)
+        .map_err(|e| Box::new(e));
+    let num: i64 = line.trim().parse()
+        .map_err(|e| Box::new(e));
     let answer = PositiveNonzeroInteger::new(num);
     answer.map_err(|e| Box::new(e))
 }
