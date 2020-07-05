fafe209fe063f60bfca31c9307a2cd39c75abe1b
dnailz <dnailzb@outlook.com>
2020-07-05 13:24:16 +0800

    autosave

--- rustlings/exercises/clippy/clippy1.rs
+++ rustlings/exercises/clippy/clippy1.rs
@@ -9,7 +9,7 @@
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
-    const eps = 0.0002f64;
+    const eps : f64 = 0.0002f64;
     if (y - x).abs() > eps {
         println!("Success!");
     }
