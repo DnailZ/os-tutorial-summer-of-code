f62ee5f38782c772229828485df804e15b16debe
dnailz <dnailzb@outlook.com>
2020-07-05 12:12:19 +0800

    autosave

--- rustlings/exercises/option/option1.rs
+++ rustlings/exercises/option/option1.rs
@@ -11,13 +11,11 @@ fn main() {
     print_number(Some(13));
     print_number(Some(99));
 
-    let mut numbers: [Option<u16>; 5];
+    let mut numbers: [Option<u16>::None; 5];
     for iter in 0..5 {
         let number_to_add: u16 = {
             ((iter * 5) + 2) / (4 * 16)
         };
-        unsafe {
-            numbers[iter as usize] = Some(number_to_add);
-        }
+        numbers[iter as usize] = Some(number_to_add);
     }
 }
