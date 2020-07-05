bcd55d6eab392128be9ab1a4ac3184d87c598559
dnailz <dnailzb@outlook.com>
2020-07-05 13:56:23 +0800

    autosave

--- rustlings/exercises/standard_library_types/iterators3.rs
+++ rustlings/exercises/standard_library_types/iterators3.rs
@@ -24,7 +24,15 @@ pub struct NotDivisibleError {
 // This function should calculate `a` divided by `b` if `a` is
 // evenly divisible by b.
 // Otherwise, it should return a suitable error.
-pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {}
+pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
+    if b == 0 {
+        return Err(DivisionError::DivideByZero);
+    }
+    if a % b != 0 {
+        return Err(DivisionError::NotDivisible);
+    }
+    a / b
+}
 
 #[cfg(test)]
 mod tests {
