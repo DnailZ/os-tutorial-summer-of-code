e8c173eac8a28891f20b079e2a91f20c4f527c08
dnailz <dnailzb@outlook.com>
2020-07-05 13:53:31 +0800

    autosave

--- rustlings/exercises/standard_library_types/iterators2.rs
+++ rustlings/exercises/standard_library_types/iterators2.rs
@@ -35,14 +35,16 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = words.iter().map(|s| capitalize_first(s)).collect();
+        let capitalized_words: Vec<String> = words.iter()
+            .map(|s| capitalize_first(s)).collect();
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
+        let capitalized_words = words.iter()
+        .map(|s| capitalize_first(s)).collect();
         assert_eq!(capitalized_words, "Hello World");
     }
 }
