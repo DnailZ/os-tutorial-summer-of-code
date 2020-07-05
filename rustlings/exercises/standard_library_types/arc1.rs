a15955104c6f532d680a0862d561dea8a01e6b18
dnailz <dnailzb@outlook.com>
2020-07-05 13:31:47 +0800

    autosave

--- rustlings/exercises/standard_library_types/arc1.rs
+++ rustlings/exercises/standard_library_types/arc1.rs
@@ -18,8 +18,8 @@ fn main() {
         joinhandles.push(thread::spawn(move || {
             let mut i = offset;
             let mut sum = 0;
-            while i < child_numbers.len() {
-                sum += child_numbers[i];
+            while i < shared_numbers.len() {
+                sum += shared_numbers[i];
                 i += 5;
             }
             println!("Sum of offset {} is {}", offset, sum);
