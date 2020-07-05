f12a5e10f89534c29b49dcb672cfa0596b8f2bfa
dnailz <dnailzb@outlook.com>
2020-07-05 11:35:21 +0800

    autosave

--- rustlings/exercises/error_handling/errors3.rs
+++ rustlings/exercises/error_handling/errors3.rs
@@ -12,7 +12,7 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input).unwrap();
+    let cost = total_cost(pretend_user_input).expect("error");
 
     if cost > tokens {
         println!("You can't afford that many!");
