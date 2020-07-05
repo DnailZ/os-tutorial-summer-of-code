e5b7adb8e8e42e9a3c016dc4a7eac3b4b5b54ede
dnailz <dnailzb@outlook.com>
2020-07-05 14:57:03 +0800

    autosave

--- rustlings/exercises/generics/generics3.rs
+++ rustlings/exercises/generics/generics3.rs
@@ -7,13 +7,13 @@
 // test pass.
 
 // I AM NOT DONE
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
     }
